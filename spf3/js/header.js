ea93dad7d00a5ba951e6f012325fc277cc6d451d
shinhee <tlsgml656@gmail.com>
2022-10-17 15:57:29 +0900

    header수정, video추가

---
+++ spf3/js/header.js
@@ -0,0 +1,8 @@
+const toogleBtn = document.querySelector('.navbar_toogleBtn');
+const menu = document.querySelector('.navbar_menu');
+const links = document.querySelector('.navbar_links');
+
+toogleBtn.addEventListener('click', () => {
+  menu.classList.toggle('active');
+  links.classList.toggle('active');
+})
