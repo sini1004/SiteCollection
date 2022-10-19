a992351a77c75a90e2dac841a2e661d88043d1a8
shinhee <tlsgml656@gmail.com>
2022-10-19 13:27:22 +0900

    header 부분 수정

--- spf3/js/header.js
+++ spf3/js/header.js
@@ -2,22 +2,22 @@
 $(function(){
   // 스크롤 시 header fade-in
   $(document).on('scroll', function(){
-      if($(window).scrollTop()>120){
-          $("#header").removeClass("deactive");
-          $("#header").addClass("active");
-          $("#logo").removeClass("logo");
-          $("#logo").addClass("logoc");
-          $("#header").css("height", "80px");
-          $("nav ul li").children("a").css("color", "#222");
-          $(".hamburger-button span").css("background", "#222");
+    if($(window).scrollTop()>120){
+        $("#header").removeClass("deactive");
+        $("#header").addClass("active");
+        $("#logo").removeClass("logo");
+        $("#logo").addClass("logoc");
+        $("#header").css("height", "80px");
+        $("nav ul li").children("a").css("color", "#222");
+        $(".hamburger-button span").css("background", "#222");
       }else{
           $("#header").removeClass("active");
           $("#header").addClass("deactive");
           $("#logo").removeClass("logoc");
           $("#logo").addClass("logo");
           $("#header").css("height", "80px");
-          $("nav ul li").children("a").css("color", "white");
-          $(".hamburger-button span").css("background", "white");
+        //   $("nav ul li").children("a").css("color", "white");
+        //   $(".hamburger-button span").css("background", "white");
       }
   });
   $('.hamburger-button').click(function(){
