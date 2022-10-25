0c319d3594b0c3c6138fa800a53f1769161e46f6
sini <tlsgml656@gmail.com>
2022-10-25 18:01:21 +0900

    category section 이미지연결 및 소스 수정

--- spf4/js/main.js
+++ spf4/js/main.js
@@ -1,5 +1,32 @@
 
-  var swiper = new Swiper(".mySwiper", {
+  var swiper1 = new Swiper(".mySwiper1", {
+    slidesPerView: 3,
+    spaceBetween: 30,
+    pagination: {
+      el: ".swiper-pagination",
+      clickable: true,
+    },
+  });
+
+  var swiper2 = new Swiper(".mySwiper2", {
+    slidesPerView: 3,
+    spaceBetween: 30,
+    pagination: {
+      el: ".swiper-pagination",
+      clickable: true,
+    },
+  });
+
+  var swiper3 = new Swiper(".mySwiper3", {
+    slidesPerView: 3,
+    spaceBetween: 30,
+    pagination: {
+      el: ".swiper-pagination",
+      clickable: true,
+    },
+  });
+
+  var swiper4 = new Swiper(".mySwiper4", {
     slidesPerView: 3,
     spaceBetween: 30,
     pagination: {
